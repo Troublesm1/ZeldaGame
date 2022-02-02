540faa40a9adb1c36978e75254ca65913688c3a1
Jovahnn Tucker <Jovahnn@yahoo.com>
2022-02-02 13:00:24 -0500

    Map added to levels.py

--- levels.py
+++ levels.py
@@ -1,13 +1,21 @@
 import pygame
+from settings import *
 
 class Level:
+
     def __init__(self):
     #GET THE DISPLAY SURFACE
         self.display_surface = pygame.display.get_surface()
-
-    #SPRITE SET-UP
+    #SPRITE GROUP SET-UP
         self.visible_sprites = pygame.sprite.Group()
         self.obstacles_sprites = pygame.sprite.Group()
+    #SPRITE SETUP
+        self.create_map()
+
+    def create_map(self):
+        for row in WORLD_MAP:
+            print(row)
+
 
     def run(self):
         #UPDATE AND DRAW GAME
