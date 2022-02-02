548f6538d2c503ee0958a38ddca4ab50e66ff678
Jovahnn Tucker <Jovahnn@yahoo.com>
2022-02-02 13:04:40 -0500

    Map Index updated

--- levels.py
+++ levels.py
@@ -13,9 +13,9 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        for row in WORLD_MAP:
+        for row_index,row in enumerate(WORLD_MAP):
+            print(row_index)
             print(row)
-
 
     def run(self):
         #UPDATE AND DRAW GAME
