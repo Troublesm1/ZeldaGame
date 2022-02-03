f7352307b575c083e3918391b947dea9cde58116
Jovahnn Tucker <Jovahnn@yahoo.com>
2022-02-02 23:39:46 -0500

    Player added and movements started

--- player.py
+++ player.py
@@ -6,3 +6,27 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load('graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
+
+        self.direction = pygame.math.Vector2()
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+                self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+                self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def update(self):
+        #UPDATE AND DRAW GAME
+        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
