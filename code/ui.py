2e3126bc3485d4a49387a1726a93bf96af8d4fe2
Jovahnn Tucker <Jovahnn@yahoo.com>
2022-02-12 10:40:04 -0500

    Additions to UI class

--- code/ui.py
+++ code/ui.py
@@ -3,7 +3,14 @@ from settings import *
 
 class UI:
     def __init__(self):
-        pass
 
-    def display(self,player):
+        # GENERAL INFO
+        self.display_surface = pygame.display.get_surface()
+        self.font = pygame.font.Font(UI_FONT, UI_FONT_SIZE)
+
+        # BAR SETUP
+        self.health_bar_rect = pygame.Rect(10, 10, HEALTH_BAR_WIDTH, BAR_HEIGHT)
+        self.energy_bar_rect = pygame.Rect(10, 34, ENERGY_BAR_WIDTH, BAR_HEIGHT)
+
+    def display(self, player):
         pass
