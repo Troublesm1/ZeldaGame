e08a59d94a49ed144cf5e281c9a9c08c12cea38c
Jovahnn Tucker <Jovahnn@yahoo.com>
2022-02-12 10:26:36 -0500

    UI class started

--- code/levels.py
+++ code/levels.py
@@ -7,6 +7,7 @@ from weapon import Weapon
 from debug import debug
 from support import *
 from random import choice
+from ui import UI
 
 
 class Level:
@@ -22,6 +23,9 @@ class Level:
 
         # SPRITE SETUP
         self.create_map()
+
+        # USER INTERFACE
+        self.ui = UI()
 
     def create_map(self):
         layouts = {
@@ -66,8 +70,7 @@ class Level:
         # UPDATE AND DRAW GAME
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.status)
-
+        self.ui.display(self.player)
 
 class YSortCameraGroup(pygame.sprite.Group):
     def __init__(self):
