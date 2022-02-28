3f9d124a41d4dc2d65ea2d37aca4167ab85f28ad
Jovahnn Tucker <Jovahnn@yahoo.com>
2022-02-27 21:53:40 -0500

    Collisions completed

--- code/levels.py
+++ code/levels.py
@@ -84,7 +84,8 @@ class Level:
                                     monster_name,
                                     (x, y),
                                     [self.visible_sprites, self.attackable_sprites],
-                                    self.obstacle_sprites)
+                                    self.obstacle_sprites,
+                                    self.damage_player)
 
     def create_attack(self):
         self.current_attack = Weapon(self.player, [self.visible_sprites, self.attack_sprites])
@@ -108,6 +109,12 @@ class Level:
                         if target_sprite.sprite_type == 'grass':
                             target_sprite.kill()
                         else:target_sprite.get_damage(self.player, attack_sprite.sprite_type,)
+
+    def damage_player(self, amount, attack_type):
+        if self.player.vulnerable:
+            self.player.health -= amount
+            self.player.vulnerable = False
+            self.player.hurt_time = pygame.time.get_ticks()
 
     def run(self):
         # UPDATE AND DRAW GAME
