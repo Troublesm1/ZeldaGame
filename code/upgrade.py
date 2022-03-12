7c42a6135bc7f64f64e47f088f3fcda80e21a442
Jovahnn Tucker <Jovahnn@yahoo.com>
2022-03-12 00:42:48 -0500

    menu names added

--- code/upgrade.py
+++ code/upgrade.py
@@ -1,6 +1,7 @@
 import pygame
 from settings import *
 
+
 class Upgrade:
     def __init__(self, player):
         # GENERAL SET-UP
@@ -8,14 +9,15 @@ class Upgrade:
         self.player = player
         self.attribute_nr = len(player.stats)
         self.attribute_names = list(player.stats.keys())
+        self.max_values = list(player.max_stats.values())
         self.font = pygame.font.Font(UI_FONT, UI_FONT_SIZE)
 
-        #ITEM CREATION
+        # ITEM CREATION
         self.height = self.display_surface.get_size()[1] * 0.8
         self.width = self.display_surface.get_size()[0] // 6
         self.create_items()
 
-        #SELECTION SYSTEM
+        # SELECTION SYSTEM
         self.selection_index = 0
         self.selection_time = None
         self.can_move = True
@@ -47,28 +49,45 @@ class Upgrade:
     def create_items(self):
         self.item_list = []
         for item, index in enumerate(range(self.attribute_nr)):
-            #HORIZONTAL POSITION
+            # HORIZONTAL POSITION
             full_width = self.display_surface.get_size()[0]
             increment = full_width // self.attribute_nr
             left = (item * increment) + (increment - self.width) // 2
-            #VERTICAL POSITION
+            # VERTICAL POSITION
             top = self.display_surface.get_size()[1] * 0.1
-            #CREATE THE OBJECT
-            item = Item(left,top,self.width,self.height,index,self.font)
+            # CREATE THE OBJECT
+            item = Item(left, top, self.width, self.height, index, self.font)
             self.item_list.append(item)
 
     def display(self):
         self.input()
         self.selection_cooldown()
 
-        for item in self.item_list:
-            item.display(self.display_surface, 0, 'test', 1, 2, 3)
+        for index, item in enumerate(self.item_list):
+
+            # get attributes
+            name = self.attribute_names[index]
+            value = self.player.get_value_by_index(index)
+            max_value = self.max_values[index]
+            cost = self.player.get_cost_by_index(index)
+            item.display(self.display_surface, self.selection_index, name, value, max_value, cost)
 
 class Item:
-    def __init__(self,l,t,w,h,index,font):
-        self.rect = pygame.Rect(l,t,w,h)
+    def __init__(self, l, t, w, h, index, font):
+        self.rect = pygame.Rect(l, t, w, h)
         self.index = index
         self.font = font
 
+    def display_names(self, surface, name, cost, selected):
+        #TITLE
+        title_surf = self.font.render(name, False, TEXT_COLOR)
+        title_rect = title_surf.get_rect(midtop=self.rect.midtop + pygame.math.Vector2(0, 20))
+
+        #COST
+
+        #DRAW
+        surface.blit(title_surf, title_rect)
+
     def display(self, surface, selection_num, name, value, max_value, cost):
         pygame.draw.rect(surface, UI_BG_COLOR, self.rect)
+        self.display_names(surface,name,cost,False)
